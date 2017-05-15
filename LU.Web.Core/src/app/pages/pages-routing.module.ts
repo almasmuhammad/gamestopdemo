70b959d2ccb0fb9a80a975ae6b3bc143d8fffc32
almasmuhammad <almasmuhammad214@gmail.com>
2017-05-15 15:20:56 -0500

    removed pages-routing and moved content to pages-routing-module.ts

---
+++ LU.Web.Core/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,24 @@
+import { Routes, RouterModule }  from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { ModuleWithProviders } from '@angular/core';
+
+export const routes: Routes = [
+  { path: 'pages', redirectTo: 'home', pathMatch: 'full' }
+
+//     path: 'pages',
+//     component: Pages,
+//     children: [
+//       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+//       { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+//       { path: 'editors', loadChildren: './editors/editors.module#EditorsModule' },
+//       { path: 'components', loadChildren: './components/components.module#ComponentsModule' },
+//       { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
+//       { path: 'ui', loadChildren: './ui/ui.module#UiModule' },
+//       { path: 'forms', loadChildren: './forms/forms.module#FormsModule' },
+//       { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
+//       { path: 'maps', loadChildren: './maps/maps.module#MapsModule' }
+//     ]
+//   }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forChild(routes);
--- LU.Web.Core/src/app/pages/pages.module.ts
+++ LU.Web.Core/src/app/pages/pages.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { routing } from './pages-routing';
+import { routing } from './pages-routing.module';
 
 import { PagesComponent } from './pages.component';
 
