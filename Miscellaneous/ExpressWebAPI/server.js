abe702eee74b77f89ea002562ce149cc76b68ffa
joeydanielallen <jda144@hotmail.com>
2017-05-15 12:40:36 -0500

    Express Web API support app
    Committing initial Express web api app

---
+++ Miscellaneous/ExpressWebAPI/server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var users = require(__dirname + '\\users.json');
+
+// app.use(function(req, res, next) {
+//   //res.header('Access-Control-Allow-Headers','*');
+//  // res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+//   if(req.method === 'OPTIONS'){
+//     res.header('Access-Control-Allow-Headers', req.headers.origin);
+//     res.header('Access-Control-Allow-Headers','*');
+//     return res.status(200).end();
+//   }
+//   next();
+// });
+
+app.use(cors());
+
+app.get('/', function(req, res){
+  res.send('<html><body>api help<br/>' +
+  'users/</body></html>');
+});
+app.get('/users/',function(req,res){
+  console.log('route request - users');
+        res.json(users);
+});
+app.get('/users/:id', function (req, res) {
+  console.log('route request - users/id');
+  if(req.params.id) {
+    var user = users["user" + req.params.id];
+    if(user){
+      res.json(user);
+    }
+    else{
+      res.status(404).end();
+    }
+  }
+});
+app.get('/unauth',function (req, res){   
+  console.log('route request - unauth');
+  res.status(401).end();});
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
+});
